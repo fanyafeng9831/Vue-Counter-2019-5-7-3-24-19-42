c7d307501bab8b965f7ee7e20bd464290d8bca48
fanyafeng9831 <1090986665@qq.com>
2019-09-02 11:23:57 +0800

    提交优化后Vue代码

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
 import Vue from 'vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  data:{
-	  counter: 0
-  },
-  methods:{
-	  add: function(){
-		  this.counter++;
-	  },
-	  sub: function(){
-		  this.counter--;
-	  }
-  }
-}).$mount('#counter')
+  render: h => h(App),
+}).$mount('#app')
